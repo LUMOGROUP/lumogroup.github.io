976d53c53874e6b17152cdaf5904862235045b9f
LUMOGROUP <36226873+LUMOGROUP@users.noreply.github.com>
2020-05-16 09:06:09 +0800

    Up ver

--- python/average.py
+++ python/average.py
@@ -1,16 +1,21 @@
 # coding: utf-8
 #Copyright (c) 2020. LSHT LLC.
-#Published on Github. https://github.com/LUMOGROUP/lumogroup.github.io/python/
-#求平均数
+#Published on Github.
+#https://GitHub.com/LUMOGROUP/lumogroup.github.io/tree/master/python/
+#求平均数 ver 0.2
 
 def pause(message="Press any key to continue..."):
     pause_pause = input(message)
 
 #`import sys
 digits = []
+i = 0
 print("求平均数\n帮助：逐个输入数字，输入完成请输入q")
 while True:
-    userin = input("输入数字>>")
+    i = i + 1
+    userin = input("输入第"
+                   + str(i)
+                   + "个数字>>")
     if userin == "q":
         break
     else:
---
+++ python/function_lib.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+#Copyright (c) 2020. LSHT LLC.
+"""
+自建函数库 实用小函数
+"""
+def help(funcion_name="help"):
+    if function_name == "help":
+        print("help [function_name] [options...]")
+
+def pause(message="Press any key to continue..."):
+    pause_pause = input(message)
+

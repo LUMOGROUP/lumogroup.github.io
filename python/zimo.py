172efba84e0fea81a8d42d18d60ef164341575ab
LUMOGROUP <36226873+LUMOGROUP@users.noreply.github.com>
2020-05-26 13:54:14 +0800

    updated zimo all-in-one function lib

--- python/zimo.py
+++ python/zimo.py
@@ -1,16 +1,27 @@
 # coding: utf-8
 #Copyright (c) 2020. LSHT LLC.
 """
-自建函数库 实用小函数
+zimo.py public function. All-in-one functioon lib.
 """
+
+
 def help(function_name="help"):
+    """help and explaintion, simple to use."""
     if function_name == "help":
         print("help [function_name] ")
-    if function_name == "":
+    elif function_name == "":
         print("help [function_name] ")
 
 def pause(message="Press any key to continue..."):
     pause_pause = input(message)
     print(pause_pause)
+
+def timestamp():
+    try:
+        import time
+        timestamp = time.time()
+        return timestamp
+    except ImportError:
+        print("moudle import failed: time")
 
 
